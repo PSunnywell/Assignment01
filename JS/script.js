b8ffdde9506c87b9683223b2d90b1e9d3f527841
NHN <noharmnow@users.noreply.github.com>
2025-02-14 20:14:20 -0800

    homework

---
+++ JS/script.js
@@ -0,0 +1,135 @@
+//STEP 1
+let someMonth
+function theMonth
+let currentMonth
+let summerMonth
+let myLibraryFunction
+
+//STEP 2
+25.5 //This is a floating point number
+'Bon Jour' //This is a string
+true
+null //This is a null value
+
+//STEP 3
+//Two examples of complex/variable expressions
+//First example
+let firstName = 'Pet';
+let lastName = 'Sunnywell';
+let fullName = firstName + " " + lastName;
+console.log(fullName); //Output: Pet Sunnywell
+
+//Second example
+let person = {firstName: 'Pet', lastName: 'Sunnywell'};
+let greeting = 'Bon Jour, ' + person.firstName + ' ' + person.lastName + '!';
+console.log(greeting); //Output: Bon Jour, Pet Sunnywell!
+
+//STEP 4
+let firstName, lastName, address, city, state, zipCode, yourAge, referralSource, mayWeContactYou;
+
+//STEP 5
+//First way to declare and assign values to variables
+let firstName;
+firstName = 'Pet';
+
+//Second way to declare and assign values to variables
+let lastName = 'Sunnywell';
+
+//Third way to declare and assign values to variables
+let zipCode = 92101, mayWeContactYou = true;
+
+//STEP 6
+let num = 5;
+let text = 'dogs';
+let result = num + " " + text;
+document.write(result); 
+
+//STEP 7
+let isLoggedIn = true;
+let userMessage = 'You are logged in: ' + isLoggedIn;
+document.write(userMessage);
+
+//STEP 8
+//The string literal is not valid.
+let someString = `Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."`;
+console.log(someString);
+
+//STEP 9
+//Variable that produces null value in console window
+let user = null;
+console.log(user);
+
+//Variable that produces undefined value in console window
+let user;
+console.log(user);
+
+//STEP 10
+console.log(typeof 'The dog is cute.'); //string
+console.log(typeof 5); //number 
+console.log(typeof true); //boolean 
+console.log(typeof { key: 'value' }); //object
+console.log(typeof undefined); //undefined
+
+//STEP 11
+let name = 'Pet Sunnywell';
+alert ('Hello ' + name + ', welcome to the JavaScript class!');
+
+//STEP 12
+var name = 'Pet Sunnywell';
+alert('Hello ' + name + ', welcome to the JavaScript class!');
+
+//STEP 13
+var course = 'JavaScript';
+alert ('Hello ' + name + ', welcome to the ' + course + ' class!');
+
+//STEP 14
+let name = 'Pet Sunnywell';
+let course = 'JavaScript';
+alert('Hello ' + name + ', \nwelcome to the ' + course + ' class!');
+
+//STEP 15
+let name = prompt('What is your name?');
+let course = 'JavaScript';
+alert('Hello ' + name + ', \nwelcome to the ' + course + ' class!');
+
+//STEP 16
+let name = prompt('What is your name?');
+let course = prompt('What class are you taking?');
+alert('Hello ' + name + ', \nwelcome to the ' + course + ' class!');
+
+//STEP 17
+var x = 10;
+var y = 20;
+console.log(x + y);
+
+//STEP 18
+var x = 20;
+x += 20;
+console.log(x);
+
+//STEP 19
+x = 20;
+x *= 5;
+console.log(x);
+
+//STEP 20
+x = 20 % 3;
+x /= 1;
+console.log(x);
+
+//STEP 21
+let x = 5;
+let y = 10;
+let result = x < y;
+console.log(result);
+
+//STEP 22
+let x = 5;
+let y = 10;
+let result = x === y;
+console.log(result);
+
+
+
+
+
